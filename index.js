d0b6a101c3983459865b68b65e61149c22defef3
Samridh Kandel <samridh.8848@gmail.com>
2021-03-30 21:44:49 +0545

    Added kick command

--- index.js
+++ index.js
@@ -33,5 +33,30 @@ client.on('message', msg => {
 });
 	
 
+client.on('message' ,msg =>{
+	if (msg.content.startsWith('kick')) {
+		const user = msg.mentions.users.first();
+		if (user){
+			const member = msg.guild.member(user);
+			if (member) {
+				member
+					.kick('Optional text to show up at audit log')
+					.then(() => {
+						msg.reply(user.tag + 'was kicked successfully');
+						
+					})
+					.catch(err => {
+						msg.reply('Unable to kick '+ user.tag);
+						console.error(err);						
+					});
+			} else {
+				message.reply("The user is not in this server");
+			}}
+			else{
+				message.reply("`No user mentioned`");
+			}
+		}
+});
+
 
 client.login('ODI2NDE5MDQyNDEwNTYxNTQ2.YGMMtQ.SfSjP90vTzFpD34w8nXHi4kDGas');
